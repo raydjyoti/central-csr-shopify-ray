85bcd3ba4cf52928ff58e523cc7a6aee3e6f7e53
raydjyoti <ray@m32.ai>
2025-10-21 13:56:26 +0530

    fix: rm old rest calls

--- web/frontend/components/ProductsCard.jsx
+++ web/frontend/components/ProductsCard.jsx
@@ -1,14 +1,9 @@
-import { useState } from "react";
 import { Card, TextContainer, Text } from "@shopify/polaris";
-import { useAppBridge } from "@shopify/app-bridge-react";
 import { useTranslation } from "react-i18next";
 import { useQuery } from "react-query";
 
 export function ProductsCard() {
-  const shopify = useAppBridge();
   const { t } = useTranslation();
-  const [isPopulating, setIsPopulating] = useState(false);
-  const productsCount = 5;
 
   const {
     data,
@@ -23,41 +18,10 @@ export function ProductsCard() {
     refetchOnWindowFocus: false,
   });
 
-  const setPopulating = (flag) => {
-    shopify.loading(flag);
-    setIsPopulating(flag);
-  };
-
-  const handlePopulate = async () => {
-    setPopulating(true);
-    const response = await fetch("/api/products", { method: "POST" });
-
-    if (response.ok) {
-      await refetchProductCount();
-
-      shopify.toast.show(
-        t("ProductsCard.productsCreatedToast", { count: productsCount })
-      );
-    } else {
-      shopify.toast.show(t("ProductsCard.errorCreatingProductsToast"), {
-        isError: true,
-      });
-    }
-
-    setPopulating(false);
-  };
-
   return (
     <Card
       title={t("ProductsCard.title")}
       sectioned
-      primaryFooterAction={{
-        content: t("ProductsCard.populateProductsButton", {
-          count: productsCount,
-        }),
-        onAction: handlePopulate,
-        loading: isPopulating,
-      }}
     >
       <TextContainer spacing="loose">
         <p>{t("ProductsCard.description")}</p>
